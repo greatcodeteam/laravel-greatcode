472983df7f574987903aaed4ea9deba5a201b759
PAM <anphamlmht@gmail.com>
2021-01-25 20:24:24 +0700

    update

---
+++ src/Provider/GreatCodeProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace GreatCode\Provider;
+
+use Illuminate\Support\ServiceProvider;
+
+class GreatCodeProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
