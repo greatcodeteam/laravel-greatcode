49e0aacb754feeb9a336501023e431d8736baf12
PAM <anphamlmht@gmail.com>
2021-01-25 21:47:45 +0700

    Update

--- src/CheckStatus.php
+++ src/CheckStatus.php
@@ -48,11 +48,7 @@ class CheckStatus
 
     public function init()
     {
-        $server_domain = '';
-        if ($_SERVER) {
-            $server_domain = parse_url($_SERVER['HTTP_HOST'])['host'];
-        }
-
+        $server_domain = $_SERVER['HTTP_HOST'] ? parse_url($_SERVER['HTTP_HOST'])['host'] : '';
         if ($this->check_path('domain')) {
             try {
                 $domain = parse_url(Storage::disk('local')->get($this->GREATCODE_STORAGE . '/domain'))['host'];
